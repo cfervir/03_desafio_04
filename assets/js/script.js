5b17b8a65338bc5b21b905ef348f758ff46975a9
Carlos Fernández <carlos@bigbadlion.com>
2022-07-15 13:11:10 -0400

    Refactorización del script para hacerlo reutilizable.

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,68 +49,55 @@ let vivienda = [
   }
 ];
 
-let datosViviendas = '';
 const contenedorViviendas = document.querySelector("#viviendas");
 
-for (let viviendas of vivienda) {
-  datosViviendas += `
-    <div class="propiedades container--flex container--propiedad">
-      <img class="propiedades__img" src="${viviendas.src}">
-      <section class="propiedades__box">
-        <h3 class="propiedades__title">${viviendas.nombre}</h3>
-        <div class="container--flex propiedades__dato">
-          <p class="propiedades__dato-interior">Cuartos: ${viviendas.cuartos}</p>
-          <p class="propiedades__dato-interior">Metros: ${viviendas.metros}</p>
-        </div>
-        <p class="propiedades__detalle">${viviendas.descripcion}</p>
-        <button class="btn btn--propiedad">Ver más</button>
-      </section>
-    </div>
-  `;
+let lasViviendas = (arrayViviendas = vivienda) => {
+
+  let datosViviendas = '';
+  document.getElementById("resultadoBuscador").textContent = `${arrayViviendas.length}`;
+
+  for (let viviendas of arrayViviendas) {
+    datosViviendas += `
+      <div class="propiedades container--flex container--propiedad">
+        <img class="propiedades__img" src="${viviendas.src}">
+        <section class="propiedades__box">
+          <h3 class="propiedades__title">${viviendas.nombre}</h3>
+          <div class="container--flex propiedades__dato">
+            <p class="propiedades__dato-interior">Cuartos: ${viviendas.cuartos}</p>
+            <p class="propiedades__dato-interior">Metros: ${viviendas.metros}</p>
+          </div>
+          <p class="propiedades__detalle">${viviendas.descripcion}</p>
+          <button class="btn btn--propiedad">Ver más</button>
+        </section>
+      </div>
+    `;
+  };
+
+  contenedorViviendas.innerHTML = datosViviendas;
+
 };
 
-contenedorViviendas.innerHTML = datosViviendas;
-document.getElementById("resultadoBuscador").textContent = `${vivienda.length}`;
-
 const nuevaBusqueda = document.getElementById('buscarViviendas');
-nuevaBusqueda.addEventListener('click', buscarViviendas);
+nuevaBusqueda.addEventListener('click', () => { buscarViviendas();  });
 
-function buscarViviendas() {
+const buscarViviendas = () => {
 
   let numeroCuartos = parseInt(document.getElementById('numeroCuartos').value);
   let desdeM2 = parseInt(document.getElementById('desdeM2').value);
   let hastaM2 = parseInt(document.getElementById('hastaM2').value);
-  let contenidoFiltrado = '';
 
-  if ((isNaN(numeroCuartos) || numeroCuartos === 0) || (isNaN(desdeM2) || desdeM2 === 0) || (isNaN(hastaM2) || hastaM2 === 0)) {
+  if ((isNaN(numeroCuartos) || numeroCuartos <= 0) || (isNaN(desdeM2) || desdeM2 <= 0) || (isNaN(hastaM2) || hastaM2 <= 0)) {
     alert('¡Ingresa valores válidos!');
   } else if (desdeM2 > hastaM2) {
     alert('Revisa los M2. Desde debe ser igual o menor que hasta.');
   } else {
-    let filtradoCuartos = vivienda.filter(function(viviendaUnica) {
-      return viviendaUnica.cuartos === numeroCuartos && viviendaUnica.metros >= desdeM2 && viviendaUnica.metros <= hastaM2;
-    });
-
-    for (let viviendasFiltradas of filtradoCuartos) {
-      contenidoFiltrado += `
-        <div class="propiedades container--flex container--propiedad">
-          <img class="propiedades__img" src="${viviendasFiltradas.src}">
-          <section class="propiedades__box">
-            <h3 class="propiedades__title">${viviendasFiltradas.nombre}</h3>
-            <div class="container--flex propiedades__dato">
-              <p class="propiedades__dato-interior">Cuartos: ${viviendasFiltradas.cuartos}</p>
-              <p class="propiedades__dato-interior">Metros: ${viviendasFiltradas.metros}</p>
-            </div>
-            <p class="propiedades__detalle">${viviendasFiltradas.descripcion}</p>
-            <button class="btn btn--propiedad">Ver más</button>
-          </section>
-        </div>
-      `;
-    };
-
-    document.getElementById("resultadoBuscador").textContent = `${filtradoCuartos.length}`;
-    contenedorViviendas.innerHTML = contenidoFiltrado;
-
+    const contenedorViviendas = vivienda.filter(viviendaUnica => (
+      viviendaUnica.cuartos === numeroCuartos && viviendaUnica.metros >= desdeM2 && viviendaUnica.metros <= hastaM2
+    ));
+    
+    lasViviendas(contenedorViviendas);
+    
   };
-
 };
+
+lasViviendas();
